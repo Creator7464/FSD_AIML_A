717c279938cc6d0c64386c9cfeaa434e0acd7956
Jaikumar Saini <jaikumar.24b1531148@abes.ac.in>
2026-07-17 14:20:16 +0530

    created ex1.js file

---
+++ ex1.js
@@ -0,0 +1,11 @@
+// var a = 10;
+// a = 20;
+// console.log("a = ", a);
+
+// function add(a,b){
+//     return a + b;
+// }
+
+// equivalent arrow function to the above function
+const add = (a,b) => a + b;
+console.log("sum = ", add(20,20));
